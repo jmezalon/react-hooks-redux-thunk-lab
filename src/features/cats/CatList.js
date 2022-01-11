2736655dede0c158ea19a42ab805bb2f4bde1699
Max Mezalon <jmezalon@gmail.com>
2022-01-11 13:29:54 -0500

    commit tree 26e78d3063ec8dbef34a5ece908f24b9f0688b2f

--- src/features/cats/CatList.js
+++ src/features/cats/CatList.js
@@ -1,1 +1,14 @@
 // write your CatList component here
+
+function CatList({ catPics = [] }) {
+  return (
+    <div>
+      {catPics.map((pic) => (
+        <img key={pic.id} src={pic.url} alt="cat" />
+      ))}
+      ;
+    </div>
+  );
+}
+
+export default CatList;
--- src/features/cats/Cats.js
+++ src/features/cats/Cats.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import CatList from "./CatList";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchCats } from "./catsSlice";
 
 function Cats() {
+  const catPics = useSelector((state) => state.entities);
+  const loading = useSelector((state) => state.status);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchCats());
+  }, [dispatch]);
+
+  if (loading === "loading") {
+    return <div>Loading...</div>;
+  }
   return (
     <div>
       <h1>CatBook</h1>
-      {/* add CatList component here */}
+      <CatList catPics={catPics} />
     </div>
   );
 }
